727ef3cdca9c7a026926587356a12b9b0fe04dff
Yung Dai <yungchidai@gmail.com>
2014-11-07 21:07:01 -0500

    Can’t get my link to create a new user to work.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,9 +3,34 @@ class UserController < ApplicationController
     @users = User.all
   end
 
+  # method to add new users
   def new
     @users = User.find(params[:id])
   end
 
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @users = User.new(user_params)
+    if @user.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to productzone_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @users.update_attribute(user_params)
+      redirect_to '/user/#{@user.id}'
+    else
+      render :edit
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   belongs_to :product_zone
+  has_many :product_zones
 end
